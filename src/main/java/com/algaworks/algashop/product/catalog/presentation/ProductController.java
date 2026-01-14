9930f49131751a00c4ab6126fa7043b08f7ff86e
soapmactavish23 <ricknogueira1231@gmail.com>
2026-01-14 16:46:51 -0300

    4.11. Contrato e recurso de listagem

--- src/main/java/com/algaworks/algashop/product/catalog/presentation/ProductController.java
+++ src/main/java/com/algaworks/algashop/product/catalog/presentation/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -52,6 +53,52 @@ public class ProductController {
                         .id(UUID.randomUUID())
                         .name("Notebook")
                         .build())
+                .build();
+    }
+
+    @GetMapping
+    public PageModel<ProductDetailOutput> filter(
+            @RequestParam(name = "size", required = false) Integer size,
+            @RequestParam(name = "number", required = false) Integer number) {
+        return PageModel.<ProductDetailOutput>builder()
+                .number(0)
+                .size(size)
+                .totalPages(1)
+                .totalElements(2)
+                .content(
+                        List.of(
+                                ProductDetailOutput.builder()
+                                        .id(UUID.randomUUID())
+                                        .addedAt(OffsetDateTime.now())
+                                        .name("Notebook X11")
+                                        .brand("Deep Diver")
+                                        .description("A Gamer Notebook")
+                                        .regularPrice(new BigDecimal("1500.00"))
+                                        .salePrice(new BigDecimal("1000.00"))
+                                        .inStock(true)
+                                        .enabled(true)
+                                        .category(CategoryMinimalOutput.builder()
+                                                .id(UUID.randomUUID())
+                                                .name("Notebook")
+                                                .build())
+                                        .build(),
+                                ProductDetailOutput.builder()
+                                        .id(UUID.randomUUID())
+                                        .addedAt(OffsetDateTime.now())
+                                        .name("Desktop I9000")
+                                        .brand("Deep Diver")
+                                        .description("A Gamer Desktop")
+                                        .regularPrice(new BigDecimal("3500.00"))
+                                        .salePrice(new BigDecimal("3000.00"))
+                                        .inStock(false)
+                                        .enabled(true)
+                                        .category(CategoryMinimalOutput.builder()
+                                                .id(UUID.randomUUID())
+                                                .name("Desktop")
+                                                .build())
+                                        .build()
+                        )
+                )
                 .build();
     }
 
