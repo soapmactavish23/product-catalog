7a9a4a3cd652f45b146045fd780730f5a745b437
soapmactavish23 <ricknogueira1231@gmail.com>
2026-04-21 15:32:11 -0300

    2.4. Atualizando e adicionando novas dependências no projeto

--- src/test/java/com/algaworks/algashop/product/catalog/contract/base/ProductBase.java
+++ src/test/java/com/algaworks/algashop/product/catalog/contract/base/ProductBase.java
@@ -4,16 +4,14 @@ import com.algaworks.algashop.product.catalog.application.PageModel;
 import com.algaworks.algashop.product.catalog.application.ResourceNotFoundException;
 import com.algaworks.algashop.product.catalog.application.product.management.ProductInput;
 import com.algaworks.algashop.product.catalog.application.product.management.ProductManagementApplicationService;
-import com.algaworks.algashop.product.catalog.application.product.query.ProductDetailOutput;
-import com.algaworks.algashop.product.catalog.application.product.query.ProductDetailOutputTestDataBuilder;
-import com.algaworks.algashop.product.catalog.application.product.query.ProductQueryService;
+import com.algaworks.algashop.product.catalog.application.product.query.*;
 import com.algaworks.algashop.product.catalog.presentation.ProductController;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
@@ -26,6 +24,8 @@ import org.springframework.web.context.WebApplicationContext;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.UUID;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 
 @WebMvcTest(controllers = ProductController.class)
 @ExtendWith(RestDocumentationExtension.class)
@@ -41,44 +41,49 @@ public class ProductBase {
     private ProductManagementApplicationService productManagementApplicationService;
 
     public static final UUID validProductId = UUID.fromString("fffe6ec2-7103-48b3-8e4f-3b58e43fb75a");
+
     public static final UUID invalidProductId = UUID.fromString("21651a12-b126-4213-ac21-19f66ff4642e");
-    public static final UUID createProductId = UUID.fromString("befb655b-dd87-4f6a-b721-d28ca85790a4");
+
+    public static final UUID createdProductId = UUID.fromString("f7c6843f-465c-476d-9a9b-4783bde4dc5e");
 
     @BeforeEach
     void setUp(RestDocumentationContextProvider documentationContextProvider) {
         RestAssuredMockMvc.mockMvc(MockMvcBuilders.webAppContextSetup(context)
-                        .apply(MockMvcRestDocumentation.documentationConfiguration(documentationContextProvider)
-                                .snippets().withTemplateFormat(TemplateFormats.asciidoctor())
-                                .and().operationPreprocessors()
-                                .withResponseDefaults(Preprocessors.prettyPrint()))
-                        .alwaysDo(MockMvcRestDocumentation.document("{ClassName}/{MethodName}"))
+                .apply(documentationConfiguration(documentationContextProvider)
+                        .snippets().withTemplateFormat(TemplateFormats.asciidoctor())
+                        .and().operationPreprocessors()
+                        .withResponseDefaults(Preprocessors.prettyPrint()))
+                .alwaysDo(MockMvcRestDocumentation.document("{ClassName}/{methodName}"))
                 .defaultResponseCharacterEncoding(StandardCharsets.UTF_8)
-                .build());
+                .build()
+        );
 
         RestAssuredMockMvc.enableLoggingOfRequestAndResponseIfValidationFails();
 
         mockValidProductFindById();
         mockFilterProducts();
         mockCreateProduct();
-        mockInvalidFindById();
+        mockInvalidProductFindById();
     }
 
-    private void mockInvalidFindById() {
+    private void mockInvalidProductFindById() {
         Mockito.when(productQueryService.findById(invalidProductId))
                 .thenThrow(new ResourceNotFoundException());
     }
 
     private void mockCreateProduct() {
         Mockito.when(productManagementApplicationService.create(Mockito.any(ProductInput.class)))
-                .thenReturn(createProductId);
-        Mockito.when(productQueryService.findById(createProductId))
+                .thenReturn(createdProductId);
+
+        Mockito.when(productQueryService.findById(createdProductId))
                 .thenReturn(ProductDetailOutputTestDataBuilder.aProduct().inStock(false).build());
     }
 
     private void mockFilterProducts() {
-        Mockito.when(productQueryService.filter(Mockito.anyInt(), Mockito.anyInt()))
-                .then((answer) -> {
-                   Integer size = answer.getArgument(0);
+        Mockito.when(productQueryService.filter(
+                        Mockito.anyInt(), Mockito.anyInt()))
+                .then((answer)-> {
+                    Integer size = answer.getArgument(0);
 
                     return PageModel.<ProductDetailOutput>builder()
                             .number(0)
