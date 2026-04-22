6716e0c14dd861a351fb5d663afaf8e3d1d6a7f8
soapmactavish23 <ricknogueira1231@gmail.com>
2026-04-22 17:03:06 -0300

    2.8. Implementando conversor de datas

--- src/main/java/com/algaworks/algashop/product/catalog/infrastructure/persistence/MongoConfig.java
+++ src/main/java/com/algaworks/algashop/product/catalog/infrastructure/persistence/MongoConfig.java
@@ -1,9 +1,17 @@
 package com.algaworks.algashop.product.catalog.infrastructure.persistence;
 
 import org.bson.UuidRepresentation;
+import org.jspecify.annotations.Nullable;
 import org.springframework.boot.mongodb.autoconfigure.MongoClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
 
 @Configuration
 public class MongoConfig {
@@ -11,6 +19,27 @@ public class MongoConfig {
     @Bean
     public MongoClientSettingsBuilderCustomizer uuidCustomizer() {
         return builder -> builder.uuidRepresentation(UuidRepresentation.STANDARD);
+    }
+
+    @Bean
+    public MongoCustomConversions customConversions() {
+       return new MongoCustomConversions(
+               List.of(new OffsetDateTimeReadConverter(), new OffsetDateTimeWroteConverter())
+       );
+    }
+
+    public static class OffsetDateTimeReadConverter implements Converter<Date, OffsetDateTime> {
+        @Override
+        public @Nullable OffsetDateTime convert(Date source) {
+            return source.toInstant().atZone(ZoneId.systemDefault()).toOffsetDateTime();
+        }
+    }
+
+    public static class OffsetDateTimeWroteConverter implements Converter<OffsetDateTime, Date> {
+        @Override
+        public @Nullable Date convert(OffsetDateTime source) {
+            return Date.from(source.toInstant());
+        }
     }
 
 }
