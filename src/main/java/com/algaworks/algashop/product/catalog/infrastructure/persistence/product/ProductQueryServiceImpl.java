ffbb0c9342b567e7722af402a3d8221cf8985f81
soapmactavish23 <ricknogueira1231@gmail.com>
2026-05-10 14:31:17 -0300

    3.12. Filtros com Or e Regex

--- src/main/java/com/algaworks/algashop/product/catalog/infrastructure/persistence/product/ProductQueryServiceImpl.java
+++ src/main/java/com/algaworks/algashop/product/catalog/infrastructure/persistence/product/ProductQueryServiceImpl.java
@@ -10,6 +10,7 @@ import com.algaworks.algashop.product.catalog.domain.model.product.Product;
 import com.algaworks.algashop.product.catalog.domain.model.product.ProductNotFoundException;
 import com.algaworks.algashop.product.catalog.domain.model.product.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoOperations;
@@ -32,6 +33,8 @@ public class ProductQueryServiceImpl implements ProductQueryService {
     private final Mapper mapper;
 
     private final MongoOperations mongoOperations;
+
+    private static final String findWordRegex = "(?i)%s";
 
     @Override
     public ProductDetailOutput findById(UUID productId) {
@@ -133,6 +136,17 @@ public class ProductQueryServiceImpl implements ProductQueryService {
             query.addCriteria(Criteria.where("categoryId").in(
                     (Object[]) filter.getCategoriesId()
             ));
+        }
+
+        if(StringUtils.isNotBlank(filter.getTerm())) {
+            String regexExpression = String.format(findWordRegex, filter.getTerm());
+            query.addCriteria(
+                    new Criteria().orOperator(
+                        Criteria.where("name").regex(regexExpression),
+                        Criteria.where("brand").regex(regexExpression),
+                        Criteria.where("description").regex(regexExpression)
+                    )
+            );
         }
 
         return query;
