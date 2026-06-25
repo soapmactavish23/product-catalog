c84eae093d0b54653b96fcf5bfc61451ad1015c8
soapmactavish23 <ricknogueira1231@gmail.com>
2026-06-24 21:11:12 -0300

    9.18. Resiliência contra indisponibilidade no servidor de cache

--- src/main/java/com/algaworks/algashop/product/catalog/infrastructure/cache/RedisCacheConfig.java
+++ src/main/java/com/algaworks/algashop/product/catalog/infrastructure/cache/RedisCacheConfig.java
@@ -1,8 +1,11 @@
 package com.algaworks.algashop.product.catalog.infrastructure.cache;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.cache.autoconfigure.RedisCacheManagerBuilderCustomizer;
+import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -12,7 +15,10 @@ import java.time.Duration;
 @Configuration
 @EnableCaching
 @ConditionalOnProperty(name = "spring.cache.type", havingValue = "redis")
-public class RedisCacheConfig {
+public class RedisCacheConfig implements CachingConfigurer {
+
+    @Autowired
+    private ResilientCacheErrorHandler resilientCacheErrorHandler;
 
     @Bean
     public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
@@ -24,4 +30,9 @@ public class RedisCacheConfig {
                         defaultCacheConfig.disableCachingNullValues().entryTtl(Duration.ofMinutes(5)));
     }
 
+    @Bean
+    @Override
+    public CacheErrorHandler errorHandler() {
+        return resilientCacheErrorHandler;
+    }
 }
