9f6da59f81e07c0608d1bed4c4e0d538984babce
soapmactavish23 <ricknogueira1231@gmail.com>
2026-06-26 15:08:25 -0300

    9.22. Cache HTTP para uma listagem com filtros

--- src/main/java/com/algaworks/algashop/product/catalog/application/category/query/CategoryDetailOutput.java
+++ src/main/java/com/algaworks/algashop/product/catalog/application/category/query/CategoryDetailOutput.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Data
@@ -17,5 +18,8 @@ public class CategoryDetailOutput implements Serializable {
     private UUID id;
     private String name;
     private Boolean enabled;
+
+    private Long version;
+    private OffsetDateTime updatedAt;
 
 }
--- src/main/java/com/algaworks/algashop/product/catalog/application/category/query/CategoryQueryService.java
+++ src/main/java/com/algaworks/algashop/product/catalog/application/category/query/CategoryQueryService.java
@@ -3,6 +3,7 @@ package com.algaworks.algashop.product.catalog.application.category.query;
 import com.algaworks.algashop.product.catalog.application.PageModel;
 import org.springframework.cache.annotation.Cacheable;
 
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 public interface CategoryQueryService {
@@ -11,4 +12,6 @@ public interface CategoryQueryService {
 
     @Cacheable(cacheNames = "algashop:categories:v1", key = "#categoryId")
     CategoryDetailOutput findById(UUID categoryId);
+
+    OffsetDateTime lastModified();
 }
