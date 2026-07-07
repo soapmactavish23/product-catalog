36c483987701569ad607af7c3a50403e90d256a7
soapmactavish23 <ricknogueira1231@gmail.com>
2026-07-07 20:55:55 -0300

    4.5. Configurando filtros globais de segurança para proteger os endpoints

--- src/main/java/com/algaworks/algashop/product/catalog/infrastructure/security/ProductCatalogSecurityConfig.java
+++ src/main/java/com/algaworks/algashop/product/catalog/infrastructure/security/ProductCatalogSecurityConfig.java
@@ -2,6 +2,7 @@ package com.algaworks.algashop.product.catalog.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,15 +13,26 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class ProductCatalogSecurityConfig {
 
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.disable())
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth ->
-                        auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/products/*/withdraw",
+                                "/api/v1/products/restock")
+                            .hasAuthority("SCOPE_products:stock:write")
+                        .requestMatchers(HttpMethod.GET ,"/api/v1/products/**")
+                            .hasAuthority("SCOPE_products:read")
+                        .requestMatchers(HttpMethod.POST ,"/api/v1/products/**")
+                            .hasAuthority("SCOPE_products:write")
+                        .requestMatchers(HttpMethod.GET ,"/api/v1/categories/**")
+                            .hasAuthority("SCOPE_categories:read")
+                        .requestMatchers(HttpMethod.POST ,"/api/v1/categories/**")
+                            .hasAuthority("SCOPE_categories:write")
+                        .requestMatchers("/actuator/health").permitAll()
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(Customizer.withDefaults()));
 
