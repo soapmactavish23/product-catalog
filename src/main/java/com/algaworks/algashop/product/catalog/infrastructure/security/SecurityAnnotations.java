c6a53ca39a0b06006e561efd4648a4aa5c7d1e9a
soapmactavish23 <ricknogueira1231@gmail.com>
2026-07-25 22:18:31 -0300

    10.6. Limitando ações por Role com PreAuthorize

--- src/main/java/com/algaworks/algashop/product/catalog/infrastructure/security/SecurityAnnotations.java
+++ src/main/java/com/algaworks/algashop/product/catalog/infrastructure/security/SecurityAnnotations.java
@@ -21,7 +21,7 @@ public class SecurityAnnotations {
 
     @Target({ElementType.METHOD, ElementType.TYPE})
     @Retention(RetentionPolicy.RUNTIME)
-    @PreAuthorize("hasAuthority('SCOPE_products:stock:write')")
+    @PreAuthorize("hasAuthority('SCOPE_products:stock:write') and hasRole('MANAGER')")
     public @interface CanWriteProductsStock {}
 
     @Target({ElementType.METHOD, ElementType.TYPE})
