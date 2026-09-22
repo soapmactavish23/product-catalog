b3a1d09c5049864751a441534a0ad54dfb4e5bdf
soapmactavish23 <ricknogueira1231@gmail.com>
2026-09-22 19:19:51 -0300

    6.5. Confirmações de recebimento (Acknowledgements)

--- src/main/java/com/algaworks/algashop/product/catalog/infrastructure/kafka/KafkaConfig.java
+++ src/main/java/com/algaworks/algashop/product/catalog/infrastructure/kafka/KafkaConfig.java
@@ -3,14 +3,23 @@ package com.algaworks.algashop.product.catalog.infrastructure.kafka;
 import com.algaworks.algashop.product.catalog.application.IntegrationEvent;
 import com.algaworks.algashop.product.catalog.application.product.event.ProductIntegrationEventPublisher;
 import com.algaworks.algashop.product.catalog.infrastructure.utility.BeanValidationUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.KafkaException;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
+@Slf4j
 @Configuration
 public class KafkaConfig {
 
@@ -30,7 +39,24 @@ public class KafkaConfig {
             BeanValidationUtil beanValidationUtil) {
         return event -> {
             beanValidationUtil.validate(event);
-            kafkaTemplate.send(properties.getProductEventTopicName(), event.getAggregateId(), event);
+            SendResult<String, Object> result = null;
+            try {
+                result = kafkaTemplate.send(properties.getProductEventTopicName(), event.getAggregateId(), event)
+                        .get(40, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            } catch (TimeoutException | ExecutionException | KafkaException e) {
+                throw new RuntimeException(e);
+            }
+
+            RecordMetadata metadata = result.getRecordMetadata();
+
+            log.info("Published {} to {}-{} at offset {}",
+                    event.getClass().getSimpleName(),
+                    metadata.topic(),
+                    metadata.partition(),
+                    metadata.offset());
         };
     }
 
